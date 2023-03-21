8a35749ee1dea766a4e69e17d12bc5b6525f4bad
Colby <colbyjcrawford@gmail.com>
2023-03-20 18:29:31 -0700

    day 5 | art gallery website

--- 03_art-gallery-website/dist/style.a6dae8f7.js
+++ 03_art-gallery-website/dist/style.a6dae8f7.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../../assets/vendors/icomoon/fonts/icomoon.eot":[["icomoon.89bacd3a.eot","../assets/vendors/icomoon/fonts/icomoon.eot"],"../assets/vendors/icomoon/fonts/icomoon.eot"],"./../../assets/vendors/icomoon/fonts/icomoon.ttf":[["icomoon.24582789.ttf","../assets/vendors/icomoon/fonts/icomoon.ttf"],"../assets/vendors/icomoon/fonts/icomoon.ttf"],"./../../assets/vendors/icomoon/fonts/icomoon.woff":[["icomoon.beda357b.woff","../assets/vendors/icomoon/fonts/icomoon.woff"],"../assets/vendors/icomoon/fonts/icomoon.woff"],"./../../assets/vendors/icomoon/fonts/icomoon.svg":[["icomoon.bd295577.svg","../assets/vendors/icomoon/fonts/icomoon.svg"],"../assets/vendors/icomoon/fonts/icomoon.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60253" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59461" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
