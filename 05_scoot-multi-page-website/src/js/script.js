c4da78ea4c82be665f7dbaaf0e14f72637d903d8
Colby <colbyjcrawford@gmail.com>
2023-04-07 12:11:41 -0700

    added pseudo decorations to images

--- 05_scoot-multi-page-website/src/js/script.js
+++ 05_scoot-multi-page-website/src/js/script.js
@@ -18,43 +18,7 @@ menuToggle.addEventListener('click', e => {
     isMenuOpen = changeBoolean(isMenuOpen);
 })
 
-// document.getElementsByName("faq1").forEach(function (el) {
-//     el.addEventListener("click", e => {
-//         if (el.getAttribute("data-waschecked") == "true") {
-//             el.checked = false;
-//             el.setAttribute("data-waschecked", "false");
-//         } else {
-//             el.checked = true;
-//             el.setAttribute("data-waschecked", "true");
-//         }
-//         disableRadioAttributes("faq1", el);
-//     });
-// });
 
-// document.getElementsByName("faq2").forEach(function (el) {
-//     el.addEventListener("click", e => {
-//         if (el.getAttribute("data-waschecked") == "true") {
-//             el.checked = false;
-//             el.setAttribute("data-waschecked", "false");
-//         } else {
-//             el.checked = true;
-//             el.setAttribute("data-waschecked", "true");
-//         }
-//         disableRadioAttributes("faq2", el);
-//     });
-// });
-
-
-function disableRadioAttributes(radioGroup, radio) {
-    var radios = document.getElementsByName(radioGroup);
-        for (let i = 0; i < radios.length; i++) {
-            if (radios[i] == radio) {
-
-            } else {
-                radios[i].setAttribute("data-waschecked", "false");
-            }
-        }
-}
 
 
 
